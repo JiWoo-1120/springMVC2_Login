25740abd682cb902155f44dc2939e63fca1e8c3e
JiWoo <didwldn97@naver.com>
2024-08-23 16:22:46 +0900

    인터셉터 컨틀롤러 경로 수정

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,9 +1,8 @@
 package hello.login;
 
-import hello.login.web.LogInterceptor;
+import hello.login.web.interceptor.LogInterceptor;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
-import lombok.extern.java.Log;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
